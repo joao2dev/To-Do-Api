8e628e07ba0631f9f403ae8e48620d111f2b6fed
joao2dev <beneficil57@gmail.com>
2026-04-04 16:21:21 -0300

    Fix: configura o flyway e adiciona a versao dele mais recente.

---
+++ src/test/java/joao2dev/To_Do_api/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package joao2dev.To_Do_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
