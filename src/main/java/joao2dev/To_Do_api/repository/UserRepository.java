6e180309a5e33676bc03ff492c16da8473f0eb3d
joao2dev <beneficil57@gmail.com>
2026-04-18 15:32:53 -0300

    Refactor: FindByLogin agora passa a receber um UserDetails

--- src/main/java/joao2dev/To_Do_api/repository/UserRepository.java
+++ src/main/java/joao2dev/To_Do_api/repository/UserRepository.java
@@ -2,7 +2,8 @@ package joao2dev.To_Do_api.repository;
 
 import joao2dev.To_Do_api.entinty.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
-    String findByEmail(String email);
+    UserDetails findByLogin(String login);
 }
